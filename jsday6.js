079f84742b561122ea550f1337488ce2198a1cb3
prathyusha-garigipati <prathyusha.garigipati@gmail.com>
2025-12-26 09:43:15 +0530

    Add files via upload
    In day 6:i have learnt while loop and do while loop

---
+++ jsday6.js
@@ -0,0 +1,16 @@
+//while loop
+let n=prompt("enter the value of n ");
+n=Number.parseInt(n);
+let i=0;
+while(i<n){
+    console.log(i);
+    i++;
+}
+//do-while loop
+let m=prompt("enter the value of m ");
+m=Number.parseInt(m);
+let j=0;
+do{
+    console.log(j);
+    j++;
+}while(j<m);
